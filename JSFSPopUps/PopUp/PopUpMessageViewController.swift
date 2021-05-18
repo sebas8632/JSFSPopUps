3f024ab330cfaf5cb8534e17b38162732c7ef225
Juan Sebastián Flórez Saavedra <sebastian8632@gmail.com>
2021-05-17 21:27:30 -0500

    Has been changed the navigation bar behaviour.

--- JSFSPopUps/PopUp/PopUpMessageViewController.swift
+++ JSFSPopUps/PopUp/PopUpMessageViewController.swift
@@ -21,6 +21,13 @@ class PopUpMessageViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     override func loadView() {
